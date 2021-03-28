3f1d17f50d3268129087ca63b85f0b5cadd42571
vundph10997 <duyvu792001@gmail.com>
2021-03-28 16:28:27 +0700

    update lnk git

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ http.createServer(function (req,res){
     res.writeHead(200,{'Content-Type' :  'text/html'});
     var url = req.url;
     if(url == '/') {
-        fs.readFile('test.html', function (error, data) {
+        fs.readFile('./Screens/index.html', function (error, data) {
             if (error == null) {
                 res.write(data);
                 res.end();
@@ -14,7 +14,7 @@ http.createServer(function (req,res){
             }
         });
     }else if(url == 'test2'){
-        fs.readFile('./Screens/test2.html',function (error,data){
+        fs.readFile('./Screens/register.html',function (error,data){
             if (error == null) {
                 res.write(data);
                 res.end();
@@ -23,4 +23,4 @@ http.createServer(function (req,res){
             }
         });
     }
-}).listen(process.env.PORT || '3000');
+}).listen(process.env.PORT || '3333');
