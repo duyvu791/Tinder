c355ae01d8f9798eedf216aa0c0ee9117443593c
vundph10997 <duyvu792001@gmail.com>
2021-04-03 11:10:14 +0700

    register

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
-http.createServer(function (req,res){
-    res.writeHead(200,{'Content-Type' :  'text/html'});
-    var url = req.url;
-    if(url == '/') {
-        fs.readFile('./Screens/index.html', function (error, data) {
-            if (error == null) {
-                res.write(data);
-                res.end();
-            } else {
-                res.end(error);
-            }
-        });
-    }else if(url == 'test2'){
-        fs.readFile('./Screens/register.html',function (error,data){
-            if (error == null) {
-                res.write(data);
-                res.end();
-            } else {
-                res.end(error);
-            }
-        });
-    }
-}).listen(process.env.PORT || '3333');
+var app = express();
+
+var expressHandlebar = require('express-handlebars');
+
+//thiết lập thư viện handlebar
+app.engine('handlebars',expressHandlebar({
+    //thiết lập thư mục, tên file layout cha
+    layoutsDir : __dirname + '/views/layouts',
+    defaultLayout: 'main',
+}))
+app.set('view engine',"handlebars")
+
+// lắng nghe http get
+app.get('/',function (request,response){
+    //var name = 'duyvu'
+    //var arr = ['apple','banana','orange'];
+    response.render('index',/*{title: name,date : '2021',arr : arr}*/)
+});
+app.use(express.static('assets'));
+
+// lắng nghe http post:
+app.post('/login',function (request,response){
+
+});
+app.listen(process.env.PORT || '2001');
